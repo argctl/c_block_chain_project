8a6970bb5ff1c27d288a8dbde37ca3cf7ed69f0e
David Kamer <me@davidkamer.com>
2024-09-19 15:56:23 -0400

    networkless container

--- rig.c
+++ rig.c
@@ -1,5 +1,20 @@
 #include "array.c"
 
+typedef struct {
+  char* code;
+  int* values;
+} gtg;
+
+int gig (char* code, int* values) {
+  gtg *gg;
+  gg->code;
+  gg->values;
+   
+}
+
+int gate (gtg *t) {
+}
+
 int main () {
   char* tar = getenv("TAR");
   array(tar);
