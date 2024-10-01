8f208ce043b19f834c7b65e79a25d4658f4b1c93
David Kamer <me@davidkamer.com>
2024-10-01 15:59:16 -0400

    loop rig to 'rig' things

--- rig.sh
+++ rig.sh
@@ -3,7 +3,9 @@ gcc rig.c -o rig
 
 sleep 2
 
-TAR=GITARGGG ./rig
-TAR=GITARGDK ./rig
-TAR=DJKDJKDK ./rig
-TAR=GITARGGG ./rig
+while true; do
+  TAR=AAAAAAAA ./rig
+  TAR=BBBBBBBB ./rig
+  TAR=CCCCCCCC ./rig
+  TAR=DDDDDDDD ./rig
+done
